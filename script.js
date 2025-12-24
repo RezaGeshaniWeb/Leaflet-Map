66d3809f71a5063ff35a4c0c9dce2b167ca7cfdd
reza geshani <rezageshaniweb@gmail.com>
2025-12-24 12:22:00 +0330

    set comment for descripton

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// set tag id
+// set Set the latitude and longitude of the location
+// Setting the initial zoom level (1, 18)
 const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
 
+// get api & set map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
