8c2c9f1892580646d8c613fe8815631948173f01
reza geshani <rezageshaniweb@gmail.com>
2025-12-24 14:49:34 +0330

    set features for map

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // set tag id
 // set Set the latitude and longitude of the location
 // Setting the initial zoom level (1, 18)
-const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
+
+/*
+const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15, {
+  animate: true,
+  duration: 2,
+});
+*/
+
+const map = L.map("map", {
+  center: [35.70015230480259, 51.33811941768732],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 13,
+  zoomControl: true,
+});
+
 
 // get api & set map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
