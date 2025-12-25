db4cd7adc21052fd0368901a6961b3c43d36865f
Reza Geshani <rezageshaniweb@gmail.com>
2025-12-25 23:49:33 +0330

    feat: add polygon & circle to markers

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
-// set tag id
-// set Set the latitude and longitude of the location
-// Setting the initial zoom level (1, 18)
 const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
 
-
-// get api & set map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
+const marker1 = L.marker([35.70067925761257, 51.334383904794876]).addTo(map)
+const marker2 = L.marker([35.702169104559204, 51.33321446165123]).addTo(map)
+const marker3 = L.marker([35.70001709448055, 51.331530034370935]).addTo(map)
+const marker4 = L.marker([35.69982539865595, 51.33806387492573]).addTo(map)
 
-// data
-const shops = [
-  {
-    position: [35.69943334081197, 51.3335470542868],
-    name: 'فروشگاه مرکزی',
-    description: 'شعبه اصلی',
-    img: 'img/shop-icon2.png',
-  },
-  {
-    position: [35.70028773592904, 51.33222538864043],
-    name: 'فروشگاه شمال',
-    description: 'شعبه شمال',
-    img: 'img/shop-icon3.png',
-  },
-]
+const line = L.polygon([
+  [35.70067925761257, 51.334383904794876],
+  [35.702169104559204, 51.33321446165123],
+  [35.70001709448055, 51.331530034370935],
+], {
+  color: 'blue',
+  fillColor: 'red',
+  fillOpacity: 0.3,
+}).addTo(map)
 
 
-// custom icon
-const storeIcon = L.icon({
-  iconUrl: 'img/shop-icon1.png',
-  iconSize: [30, 30]
-})
-
-
-// add marker & popup
-shops.forEach(shop => {
-  L.marker(shop.position, {
-    icon: storeIcon
-  })
-  .bindPopup(`
-    <img src='${shop.img}' style='width: 100%;' />
-    <h3>${shop.name}</h3>  
-    <p>${shop.description}</p>  
-  `)
-  .addTo(map)
-})
+const circle = L.circle([35.69982539865595, 51.33806387492573], {
+  radius: 200,
+  color: 'red',
+}).addTo(map)
