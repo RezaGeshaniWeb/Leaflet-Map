dc99f7257948cd672c84c3406e61705386b31406
reza geshani <rezageshaniweb@gmail.com>
2025-12-27 15:49:10 +0330

    feat: set event in map

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-const marker1 = L.marker([35.70067925761257, 51.334383904794876]).addTo(map)
-const marker2 = L.marker([35.702169104559204, 51.33321446165123]).addTo(map)
-const marker3 = L.marker([35.70001709448055, 51.331530034370935]).addTo(map)
-const marker4 = L.marker([35.69982539865595, 51.33806387492573]).addTo(map)
+map.on("click", (e) => {
+  const lat = e.latlng.lat;
+  const lng = e.latlng.lng;
+  console.log(`latlng: ${lat} , ${lng}`);
 
-const line = L.polygon([
-  [35.70067925761257, 51.334383904794876],
-  [35.702169104559204, 51.33321446165123],
-  [35.70001709448055, 51.331530034370935],
-], {
-  color: 'blue',
-  fillColor: 'red',
-  fillOpacity: 0.3,
-}).addTo(map)
+  const marker1 = L.marker(e.latlng, { draggable: true })
+    .bindPopup(`latlng: ${lat} , ${lng}`)
+    .addTo(map);
+
+  marker1.on("dragstart", (e) => {
+    console.log("start");
+  });
+
+  marker1.on("dragend", (e) => {
+    console.log("end");
+  });
+});
+
+map.on("zoomend", (e) => {
+  console.log("zoom : ", map.getZoom());
+});
+
+map.on("moveend", (e) => {
+  console.log("move : ", map.getCenter());
+});
+
+map.panTo([35.744122865962225, 51.14135870933576]);
 
 
-const circle = L.circle([35.69982539865595, 51.33806387492573], {
-  radius: 200,
-  color: 'red',
-}).addTo(map)
+// api for types of map
+// leaflet-extras.github.io/leaflet-providers/previews
+
+// thunderforest.com/maps
