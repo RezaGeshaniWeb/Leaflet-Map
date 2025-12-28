dc98abaee4321ead1bf68d97bf074da3ebd111b4
Reza Geshani <rezageshaniweb@gmail.com>
2025-12-28 22:36:07 +0330

    feat: set filter box to map

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-const map = L.map("map").setView([35.704065544779105, 51.10967164109585], 15);
+const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-// project-osrm.org
-
-const point1 = [35.704065544779105, 51.10967164109585];
-const point2 = [35.707550374942485, 51.22794632644294];
-
-const marker1 = L.marker(point1).addTo(map);
-const marker2 = L.marker(point2).addTo(map);
-
-const url = `http://router.project-osrm.org/route/v1/driving/${point1[1]},${point1[0]};${point2[1]},${point2[0]}?overview=full&geometries=geojson`;
-
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data)
-    const coordinates = data.routes[0].geometry.coordinates
-    const latLng = coordinates.map(coor => [coor[1], coor[0]])
-    L.polyline(latLng, {
-      color: 'blue',
-      weight: 4,
-      opacity: 0.7,
-    }).addTo(map)
-  });
+const geocoder = L.Control.geocoder({
+  defaultMarkGeocode: false
+}).on('markgeocode', e => {
+  console.log(e);
+  let bbox = e.geocode.bbox
+  let poly = L.polygon([
+    bbox.getSouthEast(),
+    bbox.getNorthEast(),
+    bbox.getNorthWest(),
+    bbox.getSouthWest(),
+  ]).addTo(map)
+  map.fitBounds(poly.getBounds())
+  L.marker(e.geocode.center).addTo(map).bindPopup(e.geocode.name).openPopup()
+})
+  .addTo(map)
